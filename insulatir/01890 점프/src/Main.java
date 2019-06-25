d7ebc52809cbd488a8620bd3c2f3a6031ec4249c
bluesocks <s42415466@gmail.com>
2019-06-25 16:56:33 +0900

    메모이제이션을 위한 캐쉬이며 똑같이 100x100의 크기를 가짐

--- insulatir/01890 점프/src/Main.java
+++ insulatir/01890 점프/src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 	static int[][] board = new int[100][100];
+	static long[][] cache = new long[100][100];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
