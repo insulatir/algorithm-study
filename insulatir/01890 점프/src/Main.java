65c2d8bb5f5abe38a91112ba95091cfcaeea1c42
bluesocks <s42415466@gmail.com>
2019-06-25 16:57:52 +0900

    NxN만큼 보드 위의 수를 가져오고 캐쉬를 -1로 초기화 이는 계산이 되지 않았다는 것을 나타냄

--- insulatir/01890 점프/src/Main.java
+++ insulatir/01890 점프/src/Main.java
@@ -8,7 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		
+		n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				board[i][j] = scan.nextInt();
+				cache[i][j] = -1;
+			}
+		}
 		
 		scan.close();
 	}
