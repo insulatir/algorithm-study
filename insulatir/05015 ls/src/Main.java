029077ed014e0c167de91051e187743fc0ca6e9c
bluesocks <s42415466@gmail.com>
2019-06-25 18:28:04 +0900

    패턴과 문자열

--- insulatir/05015 ls/src/Main.java
+++ insulatir/05015 ls/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Main {
 	static int[][] cache = new int[101][101];
+	static String W, S;
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
