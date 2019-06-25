e17a5aafb945721c2f558dbe04a9b395f49a6c11
bluesocks <s42415466@gmail.com>
2019-06-25 11:33:39 +0900

    입력을 위한 스캐너 코드 추가

--- insulatir/src/insulatir/Main.java
+++ insulatir/src/insulatir/Main.java
@@ -1,9 +1,15 @@
 package insulatir;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		
+		
+		scan.close();
 	}
 
 }
