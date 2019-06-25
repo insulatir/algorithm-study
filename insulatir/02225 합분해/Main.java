933ff41416c0755923dea0682c2ac34238cb179e
bluesocks <s42415466@gmail.com>
2019-06-25 14:11:08 +0900

    (N, K) = (0, K-1) + (1, K-1) + ... + (N-1, K-1) + (N, K-1) K-1개으로 이루어진 조합에 각각 0에서 N까지 더하면 (N, K)을 구할 수 있음

--- insulatir/02225 합분해/Main.java
+++ insulatir/02225 합분해/Main.java
@@ -9,6 +9,13 @@ public class Main {
 		int[][] array = new int[N + 1][K + 1];
 		
 		array[0][0] = 1;
+		for (int j = 1; j <= K; j++) {
+			for (int i = 0; i <= N; i++) {
+				for (int k = 0; k <= i; k++) {
+					array[i][j] = (array[i][j] + array[k][j-1]) % 1000000000;
+				}
+			}
+		}
 		
 		scan.close();
 	}
