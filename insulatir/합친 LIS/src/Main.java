56b8582d517ca2c5312dadc8ba9803e9aa3232ce
bluesocks <s42415466@gmail.com>
2019-06-26 14:06:13 +0900

    수열 A

--- insulatir/합친 LIS/src/Main.java
+++ insulatir/합친 LIS/src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Main {
 	static int n;
 	static int m;
+	static long[] A = new long[100];
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -10,6 +11,9 @@ public class Main {
 		for (int i = 0; i < c ; i++) {
 			n = scan.nextInt();
 			m = scan.nextInt();
+			for (int j = 0; j < n; j++) {
+				A[j] = scan.nextLong();
+			}
 		}
 		
 		scan.close();
