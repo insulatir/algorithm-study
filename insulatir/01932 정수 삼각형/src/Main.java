af35818733fdef87dbb79a51cf025bf9c3fae546
bluesocks <s42415466@gmail.com>
2019-06-26 10:35:58 +0900

    readLine()으로 한 줄을 가져온 후 int 타입으로 변환하여 삼각형의 크기를 가져옴

--- insulatir/01932 정수 삼각형/src/Main.java
+++ insulatir/01932 정수 삼각형/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		int n = Integer.parseInt(br.readLine());
+		
 		bw.flush();
 		bw.close();
 	}
