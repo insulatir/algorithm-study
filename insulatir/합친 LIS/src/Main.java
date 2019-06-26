95baa9e0752f3142147293c27b5919fd14238ed8
bluesocks <s42415466@gmail.com>
2019-06-26 14:07:59 +0900

    캐쉬 -1로 초기화

--- insulatir/합친 LIS/src/Main.java
+++ insulatir/합친 LIS/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	static int m;
 	static long[] A = new long[100];
 	static long[] B = new long[100];
+	static int[][] cache = new int[101][101];
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -18,6 +19,13 @@ public class Main {
 			for (int j = 0; j < m; j++) {
 				B[j] = scan.nextLong();
 			}
+			for (int j = 0; j < cache.length; j++) {
+				for (int k = 0; k < cache.length; k++) {
+					cache[j][k] = -1;
+				}
+			}
+			
+			
 		}
 		
 		scan.close();
