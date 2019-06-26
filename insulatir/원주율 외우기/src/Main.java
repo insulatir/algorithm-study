33b858e9097516df9de9fd3cd861073f83466cd6
bluesocks <s42415466@gmail.com>
2019-06-26 15:52:40 +0900

    수열

--- insulatir/원주율 외우기/src/Main.java
+++ insulatir/원주율 외우기/src/Main.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class Main {
-
+	static String N;
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		int C = scan.nextInt();
 		for (int i = 0; i < C; i++) {
-			
+			N = scan.next();
 		}
 		
 		scan.close();
