107b08e33a4db51d9d901602c9945c8336fb6622
bluesocks <s42415466@gmail.com>
2019-06-26 11:44:14 +0900

    퀵 정렬로 정렬

--- insulatir/11053 가장 긴 증가하는 부분 수열/src/Main.java
+++ insulatir/11053 가장 긴 증가하는 부분 수열/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
@@ -25,6 +26,8 @@ public class Main {
 				}
 			}
 		}
+		
+		Arrays.sort(len);
 		
 		scan.close();
 	}
