8cd20d10936e1352e945364fc1b3e5ca57c42a63
bluesocks <s42415466@gmail.com>
2019-06-27 11:35:18 +0900

    수열의 길이

--- insulatir/Quantization/src/Main.java
+++ insulatir/Quantization/src/Main.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class Main {
+	static int n = 0;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		int C = scan.nextInt();
 		for (int i = 0; i < C; i++) {
-			
+			n = scan.nextInt();
 		}
 		
 		scan.close();
