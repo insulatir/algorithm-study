130d2f62b3cd7f2295990c5a4e250b8b200fb53b
bluesocks <s42415466@gmail.com>
2019-06-28 14:40:28 +0900

    마을의 수

--- insulatir/두니발 박사의 탈옥/src/Main.java
+++ insulatir/두니발 박사의 탈옥/src/Main.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class Main {
+	static int n;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		int c = scan.nextInt();
 		for (int i = 0; i < c; i++) {
-			
+			n = scan.nextInt();
 		}
 		
 		scan.close();
