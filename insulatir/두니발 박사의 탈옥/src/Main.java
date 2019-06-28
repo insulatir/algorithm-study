df2eef6f20624b5fda5e9caa9fcf8d8cad1cd4ff
bluesocks <s42415466@gmail.com>
2019-06-28 14:41:20 +0900

    교도소가 있는 마을의 번호

--- insulatir/두니발 박사의 탈옥/src/Main.java
+++ insulatir/두니발 박사의 탈옥/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main {
-	static int n, d;
+	static int n, d, p;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -9,6 +9,7 @@ public class Main {
 		for (int i = 0; i < c; i++) {
 			n = scan.nextInt();
 			d = scan.nextInt();
+			p = scan.nextInt();
 		}
 		
 		scan.close();
