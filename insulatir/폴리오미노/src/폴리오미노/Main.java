54bd0d7aba68e1da5f84edb297e422c09f5e5da4
bluesocks <s42415466@gmail.com>
2019-06-28 13:30:55 +0900

    캐쉬 -1로 초기화

--- insulatir/폴리오미노/src/폴리오미노/Main.java
+++ insulatir/폴리오미노/src/폴리오미노/Main.java
@@ -1,6 +1,7 @@
 package 폴리오미노;
 import java.util.*;
 public class Main {
+	static int[][] cache = new int[101][101];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -8,6 +9,12 @@ public class Main {
 		int C = scan.nextInt();
 		for (int i = 0; i < C; i++) {
 			int n = scan.nextInt();
+			
+			for (int j = 0; j < 101; j++) {
+				for (int k = 0; k < 101; k++) {
+					cache[j][k] = -1;
+				}
+			}
 		}
 		
 		scan.close();
