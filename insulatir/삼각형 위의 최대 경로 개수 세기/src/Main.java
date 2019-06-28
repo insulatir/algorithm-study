1b7e9855c03c8b25d68ea7efdd0c9a6859aca5e6
bluesocks <s42415466@gmail.com>
2019-06-28 10:37:11 +0900

    삼각형의 크기

--- insulatir/삼각형 위의 최대 경로 개수 세기/src/Main.java
+++ insulatir/삼각형 위의 최대 경로 개수 세기/src/Main.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class Main {
+	static int n;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		int C = scan.nextInt();
 		for (int k = 0; k < C; k++) {
-			
+			n = scan.nextInt();
 		}
 		
 		scan.close();
