7e2f653ca646524c93b61ed388ce64237087899c
bluesocks <s42415466@gmail.com>
2019-06-28 13:05:28 +0900

    (d) 왼쪽 끝은 가로 타일이지만 오른쪽 끝은 세로 타일이어서 비대칭이기에 가운데 있는 타일들은 모든 경우의 수 가능

--- insulatir/비대칭 타일링/src/Main.java
+++ insulatir/비대칭 타일링/src/Main.java
@@ -54,5 +54,6 @@ public class Main {
 		cache2[width] = asymmetric2(width-2) % MOD;
 		cache2[width] = (cache2[width] + asymmetric2(width-4)) % MOD;
 		cache2[width] = (cache2[width] + tiling(width-3)) % MOD;
+		cache2[width] = (cache2[width] + tiling(width-3)) % MOD;
 	}
 }
