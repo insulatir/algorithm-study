d882b0e1db17bb3eb171cb2f0770faf71847216d
bluesocks <s42415466@gmail.com>
2019-06-28 12:27:04 +0900

    캐쉬 -1로 초기화

--- insulatir/장마가 찾아왔다/src/Main.java
+++ insulatir/장마가 찾아왔다/src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Main {
 	static int n;
 	static int m;
+	static double[][] cache = new double[1000][2001];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -10,6 +11,12 @@ public class Main {
 		for (int i = 0; i < C; i++) {
 			n = scan.nextInt();
 			m = scan.nextInt();
+			
+			for (int j = 0; j < 1000; j++) {
+				for (int k = 0; k < 2001; k++) {
+					cache[j][k] =  -1;
+				}
+			}
 		}
 		
 		scan.close();
