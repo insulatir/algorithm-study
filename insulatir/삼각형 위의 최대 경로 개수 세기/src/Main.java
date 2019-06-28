1e09a54b5a316bd9bd3d2cc76df8e4cac8534e42
bluesocks <s42415466@gmail.com>
2019-06-28 10:36:47 +0900

    케이스의 수

--- insulatir/삼각형 위의 최대 경로 개수 세기/src/Main.java
+++ insulatir/삼각형 위의 최대 경로 개수 세기/src/Main.java
@@ -4,7 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		
+		int C = scan.nextInt();
+		for (int k = 0; k < C; k++) {
+			
+		}
 		
 		scan.close();
 	}
