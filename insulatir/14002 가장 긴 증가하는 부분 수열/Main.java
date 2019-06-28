57f554615f1ea80b819a3ff98f5a7a6c3108fa16
bluesocks <s42415466@gmail.com>
2019-06-28 15:59:51 +0900

    캐쉬 -1로 초기화

--- insulatir/14002 가장 긴 증가하는 부분 수열/Main.java
+++ insulatir/14002 가장 긴 증가하는 부분 수열/Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Main {
 	static int n;
 	static int[] S = new int[1000];
+	static int[] cache = new int[1001];
+	static int[] choices = new int[1001];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -9,6 +11,11 @@ public class Main {
 		n = scan.nextInt();
 		for (int i = 0; i < n; i++) {
 			S[i] = scan.nextInt();
+		}
+		
+		for (int i = 0; i < 1001; i++) {
+			cache[i] = -1;
+			choices[i] = -1;
 		}
 		
 		scan.close();
