a4e456ba264c8698cd2f8c54ba9bee9e7b895c8b
bluesocks <s42415466@gmail.com>
2019-06-28 13:05:43 +0900

    결과 반환

--- insulatir/비대칭 타일링/src/Main.java
+++ insulatir/비대칭 타일링/src/Main.java
@@ -55,5 +55,6 @@ public class Main {
 		cache2[width] = (cache2[width] + asymmetric2(width-4)) % MOD;
 		cache2[width] = (cache2[width] + tiling(width-3)) % MOD;
 		cache2[width] = (cache2[width] + tiling(width-3)) % MOD;
+		return cache2[width];
 	}
 }
