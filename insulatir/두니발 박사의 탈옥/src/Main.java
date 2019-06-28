c992f8fbe5004f64ce716f51ccd0f57d7a5e303c
bluesocks <s42415466@gmail.com>
2019-06-28 14:40:06 +0900

    케이스의 수

--- insulatir/두니발 박사의 탈옥/src/Main.java
+++ insulatir/두니발 박사의 탈옥/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		int c = scan.nextInt();
+		for (int i = 0; i < c; i++) {
+			
+		}
+		
 		scan.close();
 	}
 
