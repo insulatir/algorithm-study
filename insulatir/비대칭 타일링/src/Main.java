0fdc26609ff2eff95d871d110b1c22a7fec8d473
bluesocks <s42415466@gmail.com>
2019-06-28 12:58:02 +0900

    케이스의 수

--- insulatir/비대칭 타일링/src/Main.java
+++ insulatir/비대칭 타일링/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		int C = scan.nextInt();
+		for (int i = 0; i < C; i++) {
+			
+		}
+		
 		scan.close();
 	}
 
