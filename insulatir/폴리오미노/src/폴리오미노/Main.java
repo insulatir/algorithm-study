05a2e7223409168352d1ce35e6830f0e1420e8e9
bluesocks <s42415466@gmail.com>
2019-06-28 13:30:04 +0900

    케이스의 수

--- insulatir/폴리오미노/src/폴리오미노/Main.java
+++ insulatir/폴리오미노/src/폴리오미노/Main.java
@@ -1,9 +1,16 @@
 package 폴리오미노;
-
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
+		
+		int C = scan.nextInt();
+		for (int i = 0; i < C; i++) {
+			
+		}
+		
+		scan.close();
 	}
 
 }
