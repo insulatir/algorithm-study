d4380450387795b2bdeb9aaeb64dc892f5bc703f
bluesocks <s42415466@gmail.com>
2019-06-28 10:38:48 +0900

    캐쉬 -1로 초기화

--- insulatir/삼각형 위의 최대 경로 개수 세기/src/Main.java
+++ insulatir/삼각형 위의 최대 경로 개수 세기/src/Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Main {
 	static int n;
 	static int[][] triangle = new int[100][100];
+	static int[][] cache = new int[100][100];
+	static int[][] countCache = new int[100][100];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -13,6 +15,13 @@ public class Main {
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j <= i; j++) {
 					triangle[i][j] = scan.nextInt();
+				}
+			}
+			
+			for (int i = 0 ; i < n; i++) {
+				for (int j = 0; j <= i; j++) {
+					cache[i][j] = -1;
+					countCache[i][j] = -1;
 				}
 			}
 		}
