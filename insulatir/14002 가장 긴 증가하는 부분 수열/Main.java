11e6091c98e71718098b4d891fbf476c63038c19
bluesocks <s42415466@gmail.com>
2019-06-28 15:59:28 +0900

    수열

--- insulatir/14002 가장 긴 증가하는 부분 수열/Main.java
+++ insulatir/14002 가장 긴 증가하는 부분 수열/Main.java
@@ -1,11 +1,15 @@
 import java.util.*;
 public class Main {
 	static int n;
+	static int[] S = new int[1000];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			S[i] = scan.nextInt();
+		}
 		
 		scan.close();
 	}
