78890191f9b86382e0900d8d7620bac4df9fda7a
bluesocks <s42415466@gmail.com>
2019-06-29 10:46:41 +0900

    캐쉬 -1로 초기화

--- insulatir/12865 평범한 배낭/src/Main.java
+++ insulatir/12865 평범한 배낭/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	static int capacity;
 	static int[] weight = new int[100];
 	static int[] value = new int[100];
+	static int[][] cache = new int[100001][1001];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -13,6 +14,12 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			weight[i] = scan.nextInt();
 			value[i] = scan.nextInt();
+		}
+		
+		for (int i = 0 ; i < 100001; i++) {
+			for (int j = 0; j < 1001; j++) {
+				cache[i][j] = -1;
+			}
 		}
 		
 		scan.close();
