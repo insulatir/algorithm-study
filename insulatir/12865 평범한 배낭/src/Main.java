f3377a8a02bc55a1648708688432021950da6057
bluesocks <s42415466@gmail.com>
2019-06-29 10:46:21 +0900

    물건의 가치

--- insulatir/12865 평범한 배낭/src/Main.java
+++ insulatir/12865 평범한 배낭/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	static int n;
 	static int capacity;
 	static int[] weight = new int[100];
+	static int[] value = new int[100];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -11,6 +12,7 @@ public class Main {
 		capacity = scan.nextInt();
 		for (int i = 0; i < n; i++) {
 			weight[i] = scan.nextInt();
+			value[i] = scan.nextInt();
 		}
 		
 		scan.close();
