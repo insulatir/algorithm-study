584d26cd1a6a97d97f61d148207c7c457fa3ff85
bluesocks <s42415466@gmail.com>
2019-06-29 10:46:04 +0900

    물건의 무게

--- insulatir/12865 평범한 배낭/src/Main.java
+++ insulatir/12865 평범한 배낭/src/Main.java
@@ -2,12 +2,16 @@ import java.util.*;
 public class Main {
 	static int n;
 	static int capacity;
+	static int[] weight = new int[100];
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		n = scan.nextInt();
 		capacity = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			weight[i] = scan.nextInt();
+		}
 		
 		scan.close();
 	}
