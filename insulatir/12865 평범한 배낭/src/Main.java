395d49e9e6e6ddb09d46e5f89cb5417a71690fa0
bluesocks <s42415466@gmail.com>
2019-06-29 10:45:40 +0900

    버틸 수 있는 무게

--- insulatir/12865 평범한 배낭/src/Main.java
+++ insulatir/12865 평범한 배낭/src/Main.java
@@ -1,11 +1,13 @@
 import java.util.*;
 public class Main {
 	static int n;
+	static int capacity;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		n = scan.nextInt();
+		capacity = scan.nextInt();
 		
 		scan.close();
 	}
